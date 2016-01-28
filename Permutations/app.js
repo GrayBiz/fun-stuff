5bfe457fa7ac37407cf718ea3a5a68b92be9fb51
GrayBiz <graybiz@karegeannes.com>
2016-01-28 03:15:41 -0500

    Initial commit

---
+++ Permutations/app.js
@@ -0,0 +1,51 @@
+var people = [
+    {
+        firstname: 'John',
+        lastname: 'Doe',
+        addresses: [
+            '111 Main Street',
+            '222 Lane Way'
+        ]
+    },
+    {
+        firstname: 'Jane',
+        lastname: 'Doe',
+        addresses: [
+            '333 walker way'
+        ]
+    }
+]
+
+
+
+
+
+
+var callPermutate = function() {
+    var word = document.getElementById('textBox').value
+    document.getElementById('output').innerHTML = '';
+    recursivePermutate(word, '');
+}
+
+function recursivePermutate(rest, soFar) {
+    var next;
+    var remaining;
+    
+    if (rest == '') {
+        
+        var div = document.getElementById("output");
+        
+        div.innerHTML = div.innerHTML + soFar + '<br />';
+        
+       
+        
+    }
+    else {
+        for (var i = 0; i < rest.length; i++) {
+            remaining = rest.substr(0,i) + rest.substr(i+1, rest.length-1);
+            next = soFar + rest[i];
+            
+            recursivePermutate(remaining, next);
+        }
+    }
+}
